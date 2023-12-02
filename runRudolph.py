97784ebc160f377cda3253d3bcf711eee2bcd756
Shalyn Nystrom <skittely@gmail.com>
2023-12-01 23:39:34 -0600

    add day 2

--- runRudolph.py
+++ runRudolph.py
@@ -1,14 +1,16 @@
 import sys
 from Solutions.day1_solution import day_1
+from Solutions.day2_solution import day_2
 
 day_map = {
-    "1": day_1
+    "1": day_1,
+    "2": day_2
 }
 
 
 def run_aoc_day():
     day_to_run = sys.argv[1]
-    sample = f"Resources/day_{day_to_run}/sample_part2.txt"
+    sample = f"Resources/day_{day_to_run}/sample.txt"
     full = f"Resources/day_{day_to_run}/full.txt"
 
     try:
