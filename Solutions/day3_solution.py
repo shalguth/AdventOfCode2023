717a36b6f29a2745d4152dfd2772e93a4b2161b3
Shalyn Nystrom <skittely@gmail.com>
2023-12-03 22:20:46 -0600

    cleanup part 1

--- Solutions/day3_solution.py
+++ Solutions/day3_solution.py
@@ -10,73 +10,42 @@ def is_int(possible_int):
 def day_3_part1(sample_file_path, full_file_path):
     schematic = []
     nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    possible_nums = []
     total_sum = 0
     num_to_ignore = 0
     with open(full_file_path, 'r') as sample_f:
         for line in sample_f:
             line = line.strip()
-            # '467..114..' -> 467, ., ., 114, ., .
-            # print("line")
-            # print(line)
             broken_line = list(line)
-            # print(broken_line)
             fixed_line = []
             big_num = ""
-            # print(broken_line)
             for i in range(len(broken_line)):
                 if broken_line[i] in nums:
                     big_num += broken_line[i]
-                    # print(big_num)
                 else:
                     if big_num != "":
                         fixed_line.extend([big_num]*len(big_num))
-                        possible_nums.append(big_num)
-                        # print("if")
-                        # print(fixed_line)
                         fixed_line.append(broken_line[i])
                         big_num = ""
                     else:
                         fixed_line.append(broken_line[i])
-                        # print("else")
-                        # print(fixed_line)
                 if i == len(broken_line) - 1:
-                    # print("last")
                     # last in line, gotta get it on the roster
                     if big_num != "":
                         fixed_line.extend([big_num]*len(big_num))
-                        possible_nums.append(big_num)
-                        # print("if if")
-                        # print(fixed_line)
-                        # fixed_line.append(broken_line[i])
-                        # print(fixed_line)
                         big_num = ""
-                    # else:
-                    #     fixed_line.append(broken_line[i])
-                    #     print("if else")
-                    #     print(fixed_line)
-            # print("final")
-            # print(fixed_line)
             schematic.append(fixed_line)
-            # print(schematic)
 
         print(schematic)
 
         for i in range(len(schematic)):  # Row
             for j in range(len(schematic[i])):  # Column
-                # print(f"location: {i},{j}")
                 if num_to_ignore == 0:
                     try:
-                        current_int = int(schematic[i][j])  #114
-                        # print(current_int)
+                        current_int = int(schematic[i][j])
                         if current_int != 0:  # We care about checking to see if we can add this or not
-                            # print(current_int)
-                            # print("ignore: ", num_to_ignore)
                             num_len = len(schematic[i][j])
                             symbols_found = False
                             for k in range(num_len):
-                                # print(current_int, k)
-                                print(f"{i}, {j}, {k}, {len(schematic[i])}")
                                 # Check if symbol is surrounding number
                                 if i == 0:
                                     if j == 0:
@@ -84,36 +53,20 @@ def day_3_part1(sample_file_path, full_file_path):
                                                (is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] == '.') and
                                                (is_int(schematic[i+1][j+1+k]) or schematic[i+1][j+1+k] == '.')):
                                             # Symbols found for this round of K
-                                            print("found symbols: 1")
                                             symbols_found = True
                                     elif j >= len(schematic[i])-num_len:
                                         if not((is_int(schematic[i][j-1+k]) or schematic[i][j-1+k] == '.') and
                                                 (is_int(schematic[i+1][j-1+k]) or schematic[i+1][j-1+k] == '.') and
                                                 (is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] == '.')):
                                             # Symbols found for this round of K
-                                            print("found symbols: 2")
                                             symbols_found = True
                                     else:
-                                        # print(f"{i}, {j}, {k}")
-                                        # print('"'+schematic[i][j-1+k]+'"')
-                                        # print(schematic[i][j-1+k] != '.')
-                                        # print((is_int(schematic[i][j-1+k]) or schematic[i][j-1+k] != '.'))
-                                        # print((is_int(schematic[i][j+1+k]) or schematic[i][j+1+k] != '.'))
-                                        # print((is_int(schematic[i+1][j-1+k]) or schematic[i+1][j-1+k] != '.'))
-                                        # print((is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] != '.'))
-                                        # print((is_int(schematic[i+1][j+1+k]) or schematic[i+1][j+1+k] != '.'))
                                         if not((is_int(schematic[i][j-1+k]) or schematic[i][j-1+k] == '.') and
                                                 (is_int(schematic[i][j+1+k]) or schematic[i][j+1+k] == '.') and
                                                 (is_int(schematic[i+1][j-1+k]) or schematic[i+1][j-1+k] == '.') and
                                                 (is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] == '.') and
                                                 (is_int(schematic[i+1][j+1+k]) or schematic[i+1][j+1+k] == '.')):
                                             # Symbols found for this round of K
-                                            # print(is_int(schematic[i][j-1+k]))
-                                            # print(is_int(schematic[i][j+1+k]))
-                                            # print(is_int(schematic[i+1][j-1+k]))
-                                            # print(is_int(schematic[i+1][j+k]))
-                                            # print(is_int(schematic[i+1][j+1+k]))
-                                            print("found symbols: 3")
                                             symbols_found = True
 
                                 elif i == len(schematic)-1:
@@ -122,7 +75,6 @@ def day_3_part1(sample_file_path, full_file_path):
                                                 (is_int(schematic[i-1][j+1+k]) or schematic[i-1][j+1+k] == '.') and
                                                 (is_int(schematic[i][j+1+k]) or schematic[i][j+1+k] == '.')):
                                             # No symbols found for this round of K
-                                            print("found symbols: 4")
                                             symbols_found = True
 
                                     elif j >= len(schematic[i])-num_len:
@@ -130,7 +82,6 @@ def day_3_part1(sample_file_path, full_file_path):
                                                 (is_int(schematic[i-1][j+k]) or schematic[i-1][j+k] == '.') and
                                                 (is_int(schematic[i][j-1+k]) or schematic[i][j-1+k] == '.')):
                                             # No symbols found for this round of K
-                                            print("found symbols: 5")
                                             symbols_found = True
                                     else:
                                         if not((is_int(schematic[i-1][j-1+k]) or schematic[i-1][j-1+k] == '.') and
@@ -139,7 +90,6 @@ def day_3_part1(sample_file_path, full_file_path):
                                                 (is_int(schematic[i][j-1+k]) or schematic[i][j-1+k] == '.') and
                                                 (is_int(schematic[i][j+1+k]) or schematic[i][j+1+k] == '.')):
                                             # No symbols found for this round of K
-                                            print("found symbols: 6")
                                             symbols_found = True
                                 else:
                                     if j == 0:
@@ -149,7 +99,6 @@ def day_3_part1(sample_file_path, full_file_path):
                                                 (is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] == '.') and
                                                 (is_int(schematic[i+1][j+1+k]) or schematic[i+1][j+1+k] == '.')):
                                             # Symbols found for this round of K
-                                            print("found symbols: 7")
                                             symbols_found = True
                                     elif j >= len(schematic[i])-num_len:
                                         if not((is_int(schematic[i-1][j-1+k]) or schematic[i-1][j-1+k] == '.') and
@@ -158,10 +107,8 @@ def day_3_part1(sample_file_path, full_file_path):
                                                 (is_int(schematic[i+1][j-1+k]) or schematic[i+1][j-1+k] == '.') and
                                                 (is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] == '.')):
                                             # Symbols found for this round of K
-                                            print("found symbols: 8")
                                             symbols_found = True
                                     else:
-                                        # print(schematic[i][j])
                                         if not((is_int(schematic[i-1][j-1+k]) or schematic[i-1][j-1+k] == '.') and
                                                 (is_int(schematic[i-1][j+k]) or schematic[i-1][j+k] == '.') and
                                                 (is_int(schematic[i-1][j+1+k]) or schematic[i-1][j+1+k] == '.') and
@@ -171,23 +118,16 @@ def day_3_part1(sample_file_path, full_file_path):
                                                 (is_int(schematic[i+1][j+k]) or schematic[i+1][j+k] == '.') and
                                                 (is_int(schematic[i+1][j+1+k]) or schematic[i+1][j+1+k] == '.')):
                                             # Symbols found for this round of K
-                                            print(f"found symbols: 9")
                                             symbols_found = True
-                            # print(symbols_found)
                             if symbols_found:
-                                # print("HERE")
                                 print(current_int)
                                 total_sum += current_int
-                                # print(total_sum)
                             num_to_ignore = num_len-1
-                            # print(f"ignore:{num_to_ignore}")
                     except ValueError:
-                        # print(f"value seeen: {schematic[i][j]}")
                         pass
 
                 else:
                     num_to_ignore -= 1
-                    # print(f"IGNORE: {num_to_ignore}")
     return total_sum
 
 
