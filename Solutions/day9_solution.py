cca5a673552345c4892ff9fcfdfcdda5dd11e09a
Shalyn Nystrom <skittely@gmail.com>
2023-12-09 16:19:11 -0600

    completed day 9

--- Solutions/day9_solution.py
+++ Solutions/day9_solution.py
@@ -1,82 +1,42 @@
-import re
-from anytree import Node
+def find_next_number(listed_numbers):
+    differences = []
+    for index in range(1, len(listed_numbers)):
+        differences.append(listed_numbers[index] - listed_numbers[index-1])
+    if differences.count(differences[0]) == len(differences):
+        # Found end / base case
+        return differences[-1]
+    return differences[-1] + find_next_number(differences)
 
-class BinaryTreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.leftChild = None
-        self.rightChild=None
+
+def find_previous_number(listed_numbers):
+    differences = []
+    for index in reversed(range(1, len(listed_numbers))):
+        differences.insert(0, listed_numbers[index] - listed_numbers[index-1])
+    if differences.count(differences[0]) == len(differences):
+        # Found end / base case
+        return differences[0]
+    return differences[0] - find_previous_number(differences)
+
 
 def day_9(sample_file_path, full_file_path):
     histories_sum = 0
-    node_lists = []
-    with open(sample_file_path, 'r') as sample_f:
+    with open(full_file_path, 'r') as sample_f:
         for line in sample_f:
             line = line.strip()
             first_round = line.split(" ")
             first_round_ints = [int(i) for i in first_round]
-
-            for index, value in enumerate(first_round_ints):
-                cur_node = BinaryTreeNode(value)
-                child_node = BinaryTreeNode(cur_node.data - prev_node.data)
-                if index == 0:
-                    cur_node = BinaryTreeNode(first_round_ints[0])
-                    node_lists.append([cur_node])
-                elif index == 1:
-                    # first item, only right child
-                    prev_node.rightChild = child_node
-                    cur_node.leftChild = child_node
-                    node_lists[0].append(cur_node)
-                    node_lists.append([child_node])
-                elif index == len(first_round)-1:
-                    # last round, only left child
-                    prev_node.rightChild = child_node
-                    node_lists[0].append(cur_node)
-                    node_lists[1].append(child_node)
-                else:
-                    prev_node.rightChild = child_node
-                    cur_node.leftChild = child_node
-                    node_lists[0].append(cur_node)
-                    node_lists[1].append(child_node)
-                prev_node = cur_node
-
-
-    # for index in range(1, len(times)):
-    #     dist_to_beat = int(distances[index])
-    #     option_count = 0
-    #     race_time = int(times[index])
-    #     # check scenarios giving higher than best distance
-    #     for ms_hold in range(int(times[index])+1):
-    #         if (ms_hold * race_time) > dist_to_beat:
-    #             option_count += 1
-    #         race_time -= 1
-    #     number_of_ways *= option_count
+            next_number_difference = find_next_number(first_round_ints)
+            histories_sum += (next_number_difference + first_round_ints[-1])
     return histories_sum
 
 
-# def day_6_part2(sample_file_path, full_file_path):
-#     number_of_ways = 1
-#     race_time = ""
-#     dist_to_beat = ""
-#     with open(full_file_path, 'r') as sample_f:
-#         time_line = sample_f.readline().strip()
-#         times = re.split(r': +| +', time_line)
-#
-#         dist_line = sample_f.readline().strip()
-#         distances = re.split(r': +| +', dist_line)
-#
-#     for index in range(1, len(times)):
-#         race_time += times[index]
-#         dist_to_beat += distances[index]
-#
-#     race_time = int(race_time)
-#     dist_to_beat = int(dist_to_beat)
-#     option_count = 0
-#
-#     # check scenarios giving higher than best distance
-#     for ms_hold in range(race_time+1):
-#         if (ms_hold * race_time) > dist_to_beat:
-#             option_count += 1
-#         race_time -= 1
-#     number_of_ways *= option_count
-#     return number_of_ways
+def day_9_part2(sample_file_path, full_file_path):
+    histories_sum = 0
+    with open(full_file_path, 'r') as sample_f:
+        for line in sample_f:
+            line = line.strip()
+            first_round = line.split(" ")
+            first_round_ints = [int(i) for i in first_round]
+            prev_number_difference = find_previous_number(first_round_ints)
+            histories_sum += (first_round_ints[0] - prev_number_difference)
+    return histories_sum
--- runRudolph.py
+++ runRudolph.py
@@ -4,7 +4,7 @@ from Solutions.day2_solution import day_2
 from Solutions.day3_solution import day_3_part1, day_3_part2
 from Solutions.day4_solution import day_4_part1, day_4_part2
 from Solutions.day6_solution import day_6, day_6_part2
-from Solutions.day9_solution import day_9
+from Solutions.day9_solution import day_9, day_9_part2
 
 day_map = {
     "1": day_1,
@@ -12,7 +12,7 @@ day_map = {
     "3": day_3_part2,
     "4": day_4_part2,
     "6": day_6_part2,
-    "9": day_9
+    "9": day_9_part2
 }
 
 
