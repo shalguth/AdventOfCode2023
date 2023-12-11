25d1240f0500a0b0f033232d1490a45a9c1c57c3
Shalyn Nystrom <skittely@gmail.com>
2023-12-10 22:46:57 -0600

    day 10 review and nope out

---
+++ Solutions/day10_solution.py
@@ -0,0 +1,42 @@
+def find_next_number(listed_numbers):
+    differences = []
+    for index in range(1, len(listed_numbers)):
+        differences.append(listed_numbers[index] - listed_numbers[index-1])
+    if differences.count(differences[0]) == len(differences):
+        # Found end / base case
+        return differences[-1]
+    return differences[-1] + find_next_number(differences)
+
+
+def find_previous_number(listed_numbers):
+    differences = []
+    for index in reversed(range(1, len(listed_numbers))):
+        differences.insert(0, listed_numbers[index] - listed_numbers[index-1])
+    if differences.count(differences[0]) == len(differences):
+        # Found end / base case
+        return differences[0]
+    return differences[0] - find_previous_number(differences)
+
+
+def day_10(sample_file_path, full_file_path):
+    histories_sum = 0
+    with open(full_file_path, 'r') as sample_f:
+        for line in sample_f:
+            line = line.strip()
+            first_round = line.split(" ")
+            first_round_ints = [int(i) for i in first_round]
+            next_number_difference = find_next_number(first_round_ints)
+            histories_sum += (next_number_difference + first_round_ints[-1])
+    return histories_sum
+
+
+def day_9_part2(sample_file_path, full_file_path):
+    histories_sum = 0
+    with open(full_file_path, 'r') as sample_f:
+        for line in sample_f:
+            line = line.strip()
+            first_round = line.split(" ")
+            first_round_ints = [int(i) for i in first_round]
+            prev_number_difference = find_previous_number(first_round_ints)
+            histories_sum += (first_round_ints[0] - prev_number_difference)
+    return histories_sum
--- runRudolph.py
+++ runRudolph.py
@@ -5,6 +5,7 @@ from Solutions.day3_solution import day_3_part1, day_3_part2
 from Solutions.day4_solution import day_4_part1, day_4_part2
 from Solutions.day6_solution import day_6, day_6_part2
 from Solutions.day9_solution import day_9, day_9_part2
+from Solutions.day10_solution import day_10 #, day_9_part2
 
 day_map = {
     "1": day_1,
@@ -12,7 +13,8 @@ day_map = {
     "3": day_3_part2,
     "4": day_4_part2,
     "6": day_6_part2,
-    "9": day_9_part2
+    "9": day_9_part2,
+    "10": day_10
 }
 
 
